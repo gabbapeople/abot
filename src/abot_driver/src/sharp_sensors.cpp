ebee6773d2e5b4b7daf459ea9c0278b2fd347af6
gabbapeople <gabbapeople@mail.ru>
2020-09-21 18:47:31 +0300

    wip-21.09.2020

--- src/abot_driver/src/sharp_sensors.cpp
+++ src/abot_driver/src/sharp_sensors.cpp
@@ -1,13 +1,16 @@
 
 #include "sharp_ir_GP2Y0A41SK.hpp"
 
-#include "std_msgs/Float32.h"
+#include <sensor_msgs/Range.h>
 #include <ros/console.h>
 #include <ros/ros.h>
 
 #define SENSOR_F_M_EXPANDER_PIN 0
 #define SENSOR_F_R_EXPANDER_PIN 1
 #define SENSOR_F_L_EXPANDER_PIN 2
+
+#define SENSOR_MIN_RANGE 0.030
+#define SENSOR_MAX_RANGE 0.370
 
 double mapClipDistance(double raw_distance) {
     double distance; 
@@ -18,6 +21,18 @@ double mapClipDistance(double raw_distance) {
     } else {
         distance = raw_distance / 100; // cm to meters
     }
+}
+
+void prepareRangeMsg(SharpIR_GP2Y0A41SK_WiringPi sensor, std::string frame, sensor_msgs::Range &range_msg) {
+
+    range_msg.header.stamp = ros::Time::now();
+    range_msg.header.frame_id = frame;
+    range_msg.radiation_type = 1;
+    range_msg.field_of_view = 0;
+    range_msg.min_range = SENSOR_MIN_RANGE;
+    range_msg.max_range = SENSOR_MAX_RANGE;
+    range_msg.range = mapClipDistance(sensor.getDistance());
+
 }
 
 int main(int argc, char **argv) {
@@ -33,27 +48,19 @@ int main(int argc, char **argv) {
 
     ros::Rate sleep_rate(50);
 
-    ros::Publisher ir_sensor_front_middle_pub = node.advertise<std_msgs::Float32>("abot/ir_sensor_front_middle", 1);
-    ros::Publisher ir_sensor_front_left_pub = node.advertise<std_msgs::Float32>("abot/ir_sensor_front_left", 1);
-    ros::Publisher ir_sensor_front_right_pub = node.advertise<std_msgs::Float32>("abot/ir_sensor_front_right", 1);
+    ros::Publisher ir_sensor_front_middle_pub = node.advertise<sensor_msgs::Range>("ir_sensor_front_middle", 1);
+    ros::Publisher ir_sensor_front_left_pub = node.advertise<sensor_msgs::Range>("ir_sensor_front_left", 1);
+    ros::Publisher ir_sensor_front_right_pub = node.advertise<sensor_msgs::Range>("ir_sensor_front_right", 1);
 
-    double raw_ir_sensor_front_middle_distance;
-    double raw_ir_sensor_front_left_distance;
-    double raw_ir_sensor_front_right_distance;
-
-    std_msgs::Float32 ir_sensor_front_middle_msg;
-    std_msgs::Float32 ir_sensor_front_left_msg;
-    std_msgs::Float32 ir_sensor_front_right_msg;
+    sensor_msgs::Range ir_sensor_front_middle_msg;
+    sensor_msgs::Range ir_sensor_front_left_msg;
+    sensor_msgs::Range ir_sensor_front_right_msg;
 
     while (ros::ok()) {
 
-        raw_ir_sensor_front_middle_distance = ir_sensor_front_middle.getDistance();
-        raw_ir_sensor_front_left_distance = ir_sensor_front_left.getDistance();
-        raw_ir_sensor_front_right_distance = ir_sensor_front_right.getDistance();
-
-        ir_sensor_front_middle_msg.data = mapClipDistance(raw_ir_sensor_front_middle_distance);
-        ir_sensor_front_left_msg.data = mapClipDistance(raw_ir_sensor_front_left_distance);
-        ir_sensor_front_right_msg.data = mapClipDistance(raw_ir_sensor_front_right_distance);
+        prepareRangeMsg(ir_sensor_front_middle, "ir_sensor_f_m", ir_sensor_front_middle_msg);
+        prepareRangeMsg(ir_sensor_front_left, "ir_sensor_f_l", ir_sensor_front_left_msg);
+        prepareRangeMsg(ir_sensor_front_right, "ir_sensor_f_r", ir_sensor_front_right_msg);
 
         ir_sensor_front_middle_pub.publish(ir_sensor_front_middle_msg);
         ir_sensor_front_left_pub.publish(ir_sensor_front_left_msg);
