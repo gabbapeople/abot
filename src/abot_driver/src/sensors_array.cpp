9b96819f53ce1364afff52f1ea34293b2b0c30df
gabbapeople <gabbapeople@mail.ru>
2020-10-05 18:54:56 +0300

    wip-02.10.2020

--- src/abot_driver/src/sensors_array.cpp
+++ src/abot_driver/src/sensors_array.cpp
@@ -9,9 +9,15 @@ int main(int argc, char **argv) {
 
 
     array.addSensor("abot/ir_sensor_f", "ir_f");
+    array.addSensor("abot/ir_sensor_fr", "ir_fr");
     array.addSensor("abot/ir_sensor_r", "ir_r");
+    array.addSensor("abot/ir_sensor_br", "ir_br");
+
+    array.addSensor("abot/ir_sensor_b", "ir_b");
+    array.addSensor("abot/ir_sensor_bl", "ir_bl");
     array.addSensor("abot/ir_sensor_l", "ir_l");
-    
+    array.addSensor("abot/ir_sensor_fl", "ir_fl");
+
     ros::spin();
 
     return 0;
